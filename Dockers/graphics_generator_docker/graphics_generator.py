1b71af290ee38668296fc663b8a3971f19f89233
Maxime Burri <maxime.burri@gmail.com>
2017-04-11 11:54:14 +0200

    Add docker graphics generator

---
+++ Dockers/graphics_generator_docker/graphics_generator.py
@@ -0,0 +1,41 @@
+import matplotlib
+# Force matplotlib to not use any Xwindows backend. To fix 
+#     _tkinter.TclError: no display name and no $DISPLAY environment variable
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+import datetime
+from collections import OrderedDict
+
+plt.style.use('seaborn-notebook')
+
+plt.figure(figsize=(20,10))
+
+# Get data
+commits = pd.read_csv('./csv_data/github_commits.csv', sep=',', parse_dates=['date'])["date"]
+issues = pd.read_csv('./csv_data/jira_issues_bugs.csv', sep=',', parse_dates=['date'])["date"]
+versions = pd.read_csv('./csv_data/jira_versions.csv', sep=',', parse_dates=['release_date'])
+
+# Print versions
+for i in versions.index :
+	date = versions["release_date"][i]
+	name = versions["name"][i]
+	released = versions["released"][i]
+	if(released):
+		plt.axvline(date, color='green', linewidth=0.5, label="Version")
+		plt.text(date+datetime.timedelta(days=10), 60, versions["name"][i], rotation=90, fontsize=8)
+
+# Print commits
+commits.hist(bins=200, label='Number of commits')
+
+# Print issues
+issues.hist(bins=200, color="red", label='Number of issues')
+
+# Print legends
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys())
+
+plt.savefig('./csv_data/all.png')

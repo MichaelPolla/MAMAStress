a75f6a7addd02aaf5fe0087f9c401b30045e25c3
Maxime Burri <maxime.burri@gmail.com>
2017-04-11 15:21:14 +0200

    Generate 3 graphics for days

--- Dockers/graphics_generator_docker/graphics_generator.py
+++ Dockers/graphics_generator_docker/graphics_generator.py
@@ -1,8 +1,3 @@
-import matplotlib
-# Force matplotlib to not use any Xwindows backend. To fix 
-#     _tkinter.TclError: no display name and no $DISPLAY environment variable
-matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import pandas as pd
@@ -11,31 +6,42 @@ from collections import OrderedDict
 
 plt.style.use('seaborn-notebook')
 
-plt.figure(figsize=(20,10))
-
 # Get data
-commits = pd.read_csv('./csv_data/github_commits.csv', sep=',', parse_dates=['date'])["date"]
-issues = pd.read_csv('./csv_data/jira_issues_bugs.csv', sep=',', parse_dates=['date'])["date"]
-versions = pd.read_csv('./csv_data/jira_versions.csv', sep=',', parse_dates=['release_date'])
+commits = pd.read_csv('github_commits.csv', sep=',', parse_dates=['date'])
+issues = pd.read_csv('jira_issues_bugs.csv', sep=',', parse_dates=['date'])
+versions = pd.read_csv('jira_versions.csv', sep=',', parse_dates=['release_date'])
 
-# Print versions
-for i in versions.index :
-	date = versions["release_date"][i]
-	name = versions["name"][i]
-	released = versions["released"][i]
-	if(released):
-		plt.axvline(date, color='green', linewidth=0.5, label="Version")
-		plt.text(date+datetime.timedelta(days=10), 60, versions["name"][i], rotation=90, fontsize=8)
+def generateFig(byFreq, filename):
+	plt.figure(figsize=(20,10))
 
-# Print commits
-commits.hist(bins=200, label='Number of commits')
+	# Print commits
+	commitsGroupBy = commits.groupby(pd.Grouper(key='date', freq=byFreq)).count()
+	plt.plot(commitsGroupBy, label="Number of commits")
+	print type(commitsGroupBy)
 
-# Print issues
-issues.hist(bins=200, color="red", label='Number of issues')
+	# Print issues
+	issuesGroupBy = issues.groupby(pd.Grouper(key='date', freq=byFreq)).count()
+	plt.plot(issuesGroupBy, label="Number of bugs", color="red")
 
-# Print legends
-handles, labels = plt.gca().get_legend_handles_labels()
-by_label = OrderedDict(zip(labels, handles))
-plt.legend(by_label.values(), by_label.keys())
+	maxValue = commitsGroupBy.max()
+	# Print versions
+	for i in versions.index :
+		date = versions["release_date"][i]
+		name = versions["name"][i]
+		released = versions["released"][i]
+		if(released):
+			plt.axvline(date, color='green', linewidth=0.5, label="Version")
+			plt.text(date+datetime.timedelta(days=10), maxValue-maxValue/5, versions["name"][i], rotation=90, fontsize=8)
 
-plt.savefig('./csv_data/all.png')
+	# Print legends
+	handles, labels = plt.gca().get_legend_handles_labels()
+	by_label = OrderedDict(zip(labels, handles))
+	plt.legend(by_label.values(), by_label.keys())
+
+	plt.savefig(filename)
+
+generateFig('1d', 'all_day.png')
+
+generateFig('7d', 'all_week.png')
+
+generateFig('1M', 'all_month.png')

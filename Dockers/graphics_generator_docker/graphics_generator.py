daa909660faf6c564e5030b5fb327def44ed1cf4
maxime <maxime.burri@gmail.com>
2017-04-12 10:49:37 +0200

    Add grphics foreach deadlines

--- Dockers/graphics_generator_docker/graphics_generator.py
+++ Dockers/graphics_generator_docker/graphics_generator.py
@@ -10,13 +10,13 @@ import datetime
 from collections import OrderedDict
 import os
 
+# Wait for other docker container
 while os.path.exists('./csv_data/github_retriever.txt')!= True:
 	pass
 while os.path.exists('./csv_data/jira_retriever.txt')!= True:
 	pass
 os.remove('./csv_data/github_retriever.txt')
 os.remove('./csv_data/jira_retriever.txt')
-
 
 plt.style.use('seaborn-notebook')
 
@@ -25,37 +25,85 @@ commits = pd.read_csv('./csv_data/github_commits.csv', sep=',', parse_dates=['da
 issues = pd.read_csv('./csv_data/jira_issues_bugs.csv', sep=',', parse_dates=['date'])
 versions = pd.read_csv('./csv_data/jira_versions.csv', sep=',', parse_dates=['release_date'])
 
-def generateFig(byFreq, filename):
-	plt.figure(figsize=(20,10))
 
-	# Print commits
-	commitsGroupBy = commits.groupby(pd.Grouper(key='date', freq=byFreq)).count()
-	plt.plot(commitsGroupBy, label="Number of commits")
-	print type(commitsGroupBy)
+def generateFig(byFreq, filename, figurename, dateBegin=None, dateEnd=None, realDate=None):
+  f,ax = None,None
+  if(dateBegin == None):
+    f=plt.figure(figsize=(20,10))
+  else:
+    f=plt.figure(figsize=(5,5))
+  
+  commitsCutted = commits
+  issuesCutted = issues
 
-	# Print issues
-	issuesGroupBy = issues.groupby(pd.Grouper(key='date', freq=byFreq)).count()
-	plt.plot(issuesGroupBy, label="Number of bugs", color="red")
+  if(dateBegin <> None):
 
-	maxValue = commitsGroupBy.max()
-	# Print versions
-	for i in versions.index :
-		date = versions["release_date"][i]
-		name = versions["name"][i]
-		released = versions["released"][i]
-		if(released):
-			plt.axvline(date, color='green', linewidth=0.5, label="Version")
-			plt.text(date+datetime.timedelta(days=10), maxValue-maxValue/5, versions["name"][i], rotation=90, fontsize=8)
+    # Cut
+    commitsCutted = commitsCutted.loc[(commitsCutted['date'] >= dateBegin) & (commitsCutted['date'] <= dateEnd)]
+    issuesCutted = issuesCutted.loc[(issuesCutted['date'] >= dateBegin) & (issuesCutted['date'] <= dateEnd)]
 
-	# Print legends
-	handles, labels = plt.gca().get_legend_handles_labels()
-	by_label = OrderedDict(zip(labels, handles))
-	plt.legend(by_label.values(), by_label.keys())
+  # Print commits and commits
+  commitsGroupBy= commitsCutted.groupby(pd.Grouper(key='date', freq=byFreq)).count()
+  issuesGroupBy = issuesCutted.groupby(pd.Grouper(key='date', freq=byFreq)).count()
 
-	plt.savefig(filename)
+  #fill_in_missing_dates
+  if dateBegin<>None:
+    idx = pd.date_range(start=dateBegin, end=dateEnd, freq="D")
+    for d in idx:
+      try: 
+        a = commitsGroupBy.loc[d]  
+      except :
+        commitsGroupBy.loc[d] = 0
+      """try: 
+        a = issuesGroupBy.loc[d]  
+      except :
+        issuesGroupBy.loc[d] = 0"""
+    
+  commitsGroupBy.sort_index()
+  issuesGroupBy.sort_index()
 
-generateFig('1d', './csv_data/all_day.png')
+  ax = commitsGroupBy.plot(color='blue', legend=False, sharex=True)
+  try:
+    issuesGroupBy.plot(ax = ax, color='red', legend=False, sharex=True)
+  except:
+    print "Nothing"
+   
+  maxValue = commitsGroupBy.max()
+  if(dateBegin == None):
+    # Print versions
+    for i in versions.index :
+      date = versions["release_date"][i]
+      name = versions["name"][i]
+      released = versions["released"][i]
+      if(released):
+        plt.axvline(date, color='green', linewidth=0.5, label="Version")
+        plt.text(date+datetime.timedelta(days=4), maxValue-maxValue/5, versions["name"][i], rotation=90, fontsize=8)
+  else:
+    plt.axvline(realDate, color='green', linewidth=0.5, label="Deadline")
 
-generateFig('7d', './csv_data/all_week.png')
+  f.autofmt_xdate()
+  
+  # Print legends
+  handles, labels = plt.gca().get_legend_handles_labels()
+  by_label = OrderedDict(zip(labels, handles))
+  plt.suptitle(figurename, fontsize=20)
+  if(dateEnd <> None):
+    plt.figtext(.5,.905, realDate.strftime("%d/%m/%Y"), fontsize=10, ha='center')
+  plt.legend(['Commits', 'Bugs', 'Deadlines'], loc='upper left')
+  plt.savefig(filename)
 
-generateFig('1M', './csv_data/all_month.png')
+# Generate general graphics
+generateFig('1d', './csv_data/all_day.png', "Par jour")
+generateFig('7d', './csv_data/all_week.png', "Par semaine")
+generateFig('1M', './csv_data/all_month.png', "Par mois")
+
+# Generate graphics foreach release
+for i in versions.index :
+  date = versions["release_date"][i]
+  name = versions["name"][i]
+  released = versions["released"][i]
+  if(released):
+    beginDate = date - datetime.timedelta(days=14)
+    print name + " from:"+str(beginDate) + " to:" + str(date)
+    dateEnd = date + datetime.timedelta(days=14)
+    generateFig('1d', './csv_data/' + name+'.png', name, beginDate, dateEnd, date)
